5f575fdcadf0dce045e025cffd65569725ea9fb1
vishnu-manager <mvishnu970m@gmail.com>
2025-07-18 11:58:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for, flash
+from flask import Flask, render_template, request, redirect, session, url_for, flash, jsonify
 from werkzeug.utils import secure_filename
 
 import psycopg2
